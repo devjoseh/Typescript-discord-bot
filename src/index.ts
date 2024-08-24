4c3e8514e1a4885b87c7c303327cbba1807130e8
José Hernanes <68444005+devjoseh@users.noreply.github.com>
2024-08-24 18:46:15 -0300

    refactor: new structure

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,15 @@
 import { Client, Collection, GatewayIntentBits, Partials } from "discord.js";
-import config from './configs/config'
+import dotenv from 'dotenv'
+import chalk from 'chalk'
 import * as path from 'path'
 import * as fs from 'fs'
+
+console.log(chalk.yellow(`███╗   ██╗███████╗███████╗████████╗    ███████╗████████╗ ██████╗ ██████╗ ███████╗`))
+console.log(chalk.yellow(`████╗  ██║██╔════╝██╔════╝╚══██╔══╝    ██╔════╝╚══██╔══╝██╔═══██╗██╔══██╗██╔════╝`))
+console.log(chalk.yellow(`██╔██╗ ██║█████╗  ███████╗   ██║       ███████╗   ██║   ██║   ██║██████╔╝█████╗ `))
+console.log(chalk.yellow(`██║╚██╗██║██╔══╝  ╚════██║   ██║       ╚════██║   ██║   ██║   ██║██╔══██╗██╔══╝  `))
+console.log(chalk.yellow(`██║ ╚████║███████╗███████║   ██║       ███████║   ██║   ╚██████╔╝██║  ██║███████╗`))
+console.log(chalk.yellow(`╚═╝  ╚═══╝╚══════╝╚══════╝   ╚═╝       ╚══════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝`))
 
 const client: any = new Client({
     intents: [ GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildEmojisAndStickers, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildInvites, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildPresences, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildMessageTyping, GatewayIntentBits.DirectMessages, GatewayIntentBits.DirectMessageReactions, GatewayIntentBits.DirectMessageTyping, GatewayIntentBits.MessageContent ],
@@ -9,8 +17,9 @@ const client: any = new Client({
     partials: [ Partials.Message, Partials.Channel, Partials.GuildMember, Partials.Reaction, Partials.GuildScheduledEvent, Partials.User, Partials.ThreadMember ]
 });
 
+dotenv.config();
+
 client.slashCommands = new Collection()
-client.config = config;
 export default client;
 
 client.setMaxListeners(0);
@@ -21,4 +30,4 @@ client.categories = fs.readdirSync(slashCommandsPath);
     require(`./handlers/${handler}`)(client);
 });
 
-client.login(config.token);
+client.login(process.env.TOKEN);
